107d5fa95b19d0740e59c7985e662721f66d5cbb
Betsy Vidal <betsy.vidal@tektonlabs.com>
2023-01-11 09:38:53 -0500

    feat: Create getProductById controller

--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -1,8 +1,16 @@
 import path from 'path';
 import { promises as fs } from 'fs';
+import { Product } from '../interfaces/product';
 
 export const getAllProducts = async () => {
   const jsonDirectory = path.join(process.cwd(), 'data');
   const fileContents = await fs.readFile(jsonDirectory + '/products.json', 'utf8');
-  return fileContents;
+  return JSON.parse(fileContents);
+};
+
+export const getProductById = async (id: string) => {
+  const products = await getAllProducts();
+  const product = products.find((product: Product) => product.productId === parseInt(id, 10))  
+  
+  return product;
 };
